271873e9a5993739f7af4d143260def4df88c6f9
El Taha, Nadim <nadimeltaha@gmail.com>
2015-04-10 12:04:22 +0200

    response with files already segmented and with priority order

---
+++ bin/resources.js
@@ -0,0 +1,40 @@
+module.exports = [
+  {
+    id: "1b305d585b1918f297164add46784116",
+    name: "ubuntu-14.04.2-desktop-amd64.iso",
+    segments: [
+      {
+        digest: '0dad187e760636d11563b452f2d0a642',
+        candidates: [
+          "http://localhost:4001/videos/1b305d585b1918f297164add46784116/ubuntu-14.04.2-desktop-amd64.iso.aa",
+          "http://localhost:4002/videos/1b305d585b1918f297164add46784116/ubuntu-14.04.2-desktop-amd64.iso.aa",
+          "http://localhost:4003/videos/1b305d585b1918f297164add46784116/ubuntu-14.04.2-desktop-amd64.iso.aa"
+        ]
+      },
+      {
+        digest: '1d2efc2d2d1ce87157905ba0e5906929',
+        candidates: [
+          "http://localhost:4002/videos/1b305d585b1918f297164add46784116/ubuntu-14.04.2-desktop-amd64.iso.ab",
+          "http://localhost:4003/videos/1b305d585b1918f297164add46784116/ubuntu-14.04.2-desktop-amd64.iso.ab",
+          "http://localhost:4001/videos/1b305d585b1918f297164add46784116/ubuntu-14.04.2-desktop-amd64.iso.ab"
+        ]
+      },
+      {
+        digest: 'b23f23ecde91580904989ea33cd1e478',
+        candidates: [
+          "http://localhost:4003/videos/1b305d585b1918f297164add46784116/ubuntu-14.04.2-desktop-amd64.iso.ac",
+          "http://localhost:4001/videos/1b305d585b1918f297164add46784116/ubuntu-14.04.2-desktop-amd64.iso.ac",
+          "http://localhost:4002/videos/1b305d585b1918f297164add46784116/ubuntu-14.04.2-desktop-amd64.iso.ac"
+        ]
+      },
+      {
+        digest: 'ccc4d993d2de6dae99d120f99f25ca86',
+        candidates: [
+          "http://localhost:4001/videos/1b305d585b1918f297164add46784116/ubuntu-14.04.2-desktop-amd64.iso.ad",
+          "http://localhost:4002/videos/1b305d585b1918f297164add46784116/ubuntu-14.04.2-desktop-amd64.iso.ad",
+          "http://localhost:4003/videos/1b305d585b1918f297164add46784116/ubuntu-14.04.2-desktop-amd64.iso.ad"
+        ]
+      }
+    ]
+  }, // Another resources
+];
--- routes/files.js
+++ routes/files.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var debug = require('debug')('server:files');
 
+var files = require('../bin/resources');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json(files);
@@ -27,44 +29,5 @@ router.get('/:id', function(req, res, next) {
 	res.json(req.file);
 	debug(JSON.stringify(req.file));
 });
-
-var files = [
-	{
-		id: "c4580a1bff257c7f956e7e8239cbce9f",
-		name: "Sintel HD 1080p",
-		candidates: [
-			{
-				url: "http://localhost:4001/videos/Sintel.2010.1080p.mkv",
-				priority: 0
-			},
-			{
-				url: "http://localhost:4002/videos/Sintel.2010.1080p.mkv",
-				priority: 1
-			},
-			{
-				url: "http://localhost:4003/videos/Sintel.2010.1080p.mkv",
-				priority: 2
-			}
-		],
-	}, 
-	{
-		id: "d2e1e3dca3cbe3d0d226e0fdd03a1900",
-		name: "Sintel HD 720p",
-		candidates: [
-			{
-				url: "http://localhost:4001/videos/Sintel.2010.720p.mkv",
-				priority: 1
-			},
-			{
-				url: "http://localhost:4002/videos/Sintel.2010.720p.mkv",
-				priority: 2
-			},
-			{
-				url: "http://localhost:4003/videos/Sintel.2010.720p.mkv",
-				priority: 0
-			}
-		],
-	}
-];
 
 module.exports = router;
