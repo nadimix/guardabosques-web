c273fa4890e29c0b47e5b4aedefb30202ada4e20
El Taha, Nadim <nadimeltaha@gmail.com>
2015-04-10 22:55:15 +0200

    Adds download chunk and insert into IndexedDB

--- public/javascripts/guardabosques.js
+++ public/javascripts/guardabosques.js
@@ -37,8 +37,110 @@ function downloadHandler(resource) {
   console.log(JSON.stringify(helper));
   setQueue(helper);
   console.log(queue);
+
+  // TODO gestionar queue (downloadFile(url, index))
+  var i = 0;
+  queue.forEach(function(url) {
+    downloadFile(url, i);
+    i++;
+  });
 }
 // End download handler
+
+// Downloads a single file into a blob
+function downloadFile(url, index) {
+  var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.OIndexedDB || window.msIndexedDB;
+  var IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.OIDBTransaction || window.msIDBTransaction;
+  var dbVersion = 1.0;
+
+  var database;
+
+  var connect = indexedDB.open("guardabosquesDB2", dbVersion);
+  var objectStore = 'chunks';
+
+  // IndexedDB connection handlers
+  connect.onerror = function(event) {
+    console.log("Error creating/accessing IndexedDB database");
+  };
+
+  connect.onsuccess = function(event) {
+    console.log("Success creating/accessing IndexedDB database");
+
+    database = connect.result;
+    database.onerror = function (event) {
+      console.log("Error creating/accessing IndexedDB database");
+    };
+
+    // Provisional solution for Chrome using objectStore instead. Will be deprecated
+    if (database.setVersion) {
+      if (database.version != dbVersion) {
+        var setVersion = database.setVersion(dbVersion);
+        setVersion.onsuccess = function () {
+          createObjectStore(database);
+          console.log('true');
+          getChunkFile(url, index);
+        };
+      }
+      else {
+        console.log('true');
+        getChunkFile(url, index);
+      }
+    }
+    else {
+      console.log('true');
+      getChunkFile(url, index);
+    }
+  };
+
+  // For future use. Currently only in latest Firefox versions
+  connect.onupgradeneeded = function (event) {
+    createObjectStore(event.target.result);
+  };
+  // End IndexedDB connection handlers
+
+  // Create/open database
+  function createObjectStore (database) {
+      // Create an objectStore
+      console.log("Creating objectStore")
+      database.createObjectStore(objectStore);
+  }
+
+  function getChunkFile(url, index) {
+    // Create XHR
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", url, true);
+    // Set the responseType to blob
+    xhr.responseType = "blob";
+
+    xhr.addEventListener("load", function () {
+      if (xhr.status === 200) {
+        console.log("chunk retrieved");
+
+        // Blob as response
+        var blob = xhr.response;
+        console.log("Blob:" + blob + "size: " + blob.size);
+
+        // Put the received blob into IndexedDB
+        putChunkInDB(blob, index);
+      }
+    }, false);
+    // Send XHR
+    xhr.send();
+  }
+
+  function putChunkInDB(blob, index) {
+    console.log("Putting elephants in IndexedDB");
+
+    // Open a transaction to the database
+    var readWriteMode = typeof IDBTransaction.READ_WRITE == "undefined" ? "readwrite" : IDBTransaction.READ_WRITE;
+    var transaction = database.transaction([objectStore], readWriteMode);
+
+    // Put the blob into the database
+    console.log('index: ' + index);
+    var put = transaction.objectStore(objectStore).put(blob, index);
+  }
+}
 
 // Setters
 function setHelper(resource) {
