e0a6f2f20850fdfdf630c39306802bfe1f5e735c
El Taha, Nadim <nadimeltaha@gmail.com>
2015-04-26 02:09:14 +0200

    Removes digest from the candidates

--- bin/resources.js
+++ bin/resources.js
@@ -5,7 +5,6 @@ module.exports = [
     chunks: [
       {
         id: 0,
-        digest: '211d38f3ff1971515be63ab912f2820f',
         candidates: [
           "http://localhost:4001/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.aa",
           "http://localhost:4002/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.aa",
@@ -14,7 +13,6 @@ module.exports = [
       },
       {
         id: 1,
-        digest: '51b33fe12ca02e6b2de024dc6df1ad3e',
         candidates: [
           "http://localhost:4002/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ab",
           "http://localhost:4003/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ab",
@@ -22,7 +20,6 @@ module.exports = [
         ]
       }, {
         id: 2,
-        digest: 'a1bd7ec5b66d0a441939328352a10bda',
         candidates: [
           "http://localhost:4003/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ac",
           "http://localhost:4001/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ac",
@@ -31,7 +28,6 @@ module.exports = [
       },
       {
         id: 3,
-        digest: '513a38e500a103ef62cc534a4cbcabe9',
         candidates: [
           "http://localhost:4001/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ad",
           "http://localhost:4002/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ad",
@@ -40,7 +36,6 @@ module.exports = [
       },
       {
         id: 4,
-        digest: 'b2cb14de21c884fdc30b6c12f2b721d1',
         candidates: [
           "http://localhost:4001/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ae",
           "http://localhost:4002/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ae",
@@ -49,7 +44,6 @@ module.exports = [
       },
       {
         id: 5,
-        digest: '9cffc41fdd9d20f66b23ff1ab8afd32c',
         candidates: [
           "http://localhost:4002/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.af",
           "http://localhost:4003/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.af",
@@ -57,16 +51,14 @@ module.exports = [
         ]
       }, {
         id: 6,
-        digest: 'ee528221500b7e4de0eefde544ada435',
         candidates: [
           "http://localhost:4003/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ag",
-          "http://localhost:4001/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ag",
-          "http://localhost:4002/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ag"
+          "http://localhost:4002/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ag",
+          "http://localhost:4001/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ag"
         ]
       },
       {
         id: 7,
-        digest: 'b2b59b333c24ec8975ad696d714776c2',
         candidates: [
           "http://localhost:4001/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ah",
           "http://localhost:4002/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ah",
@@ -75,7 +67,6 @@ module.exports = [
       },
       {
         id: 8,
-        digest: '16681835170c30cd3e92d35f62b3b82b',
         candidates: [
           "http://localhost:4002/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ai",
           "http://localhost:4003/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.ai",
@@ -83,7 +74,6 @@ module.exports = [
         ]
       }, {
         id: 9,
-        digest: '93e4749b10fed918f961df536579ac4b',
         candidates: [
           "http://localhost:4003/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.aj",
           "http://localhost:4001/files/0db391f587a4610373fbb714c05d2456/debian-7.8.0-amd64-i386-netinst.iso.aj",
